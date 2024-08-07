fcec77cd9c1917387cc6b78eafd65eedd4b73602
nane-petrosyan <nane.t.petrosyan@gmail.com>
2024-08-07 18:41:25 +0400

    Fixing version conflicts.

--- src/main/java/org/nanulik/configuration/DynamicDataSourceConfig.java
+++ src/main/java/org/nanulik/configuration/DynamicDataSourceConfig.java
@@ -6,13 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Nane Petrosyan
